e4137a6fcfb9f71dc10cda9e574502e58fbb0017
Arielle B Cruz <arielle.cruz@gmail.com>
2013-08-19 05:14:30 +0800

    Initial Commit (Really)

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-mako-templates-server
-=====================
+# Modified Mako Templates Test Server
+
+A simple webserver for testing Mako templates for HTML and JSON.  
+It also has the ability to serve static files. But please do not  
+use this in a production environment, unless you're crazy.  
+  
+makoserv.py is just a jacked-up version of the run_wsgi.py script  
+that comes with a standard Mako install. Modifications are marked  
+by “S13: description of mod”.
+  
+  
+## Usage
+Install Mako:
+
+    $ pip install mako
+  
+Go to your project's directory:
+
+    $ cd /path/to/your/project/directory
+
+Start serving on http://0.0.0.0:5000:
+
+    /path/to/your/project/directory$ python /path/to/makoserv.py
+
+Or serve on a custom ipaddress and port (both are required):
+
+    /path/to/your/project/directory$ python /path/to/makoserv.py &lt;ip_address&gt;:&lt;port_number&gt;
+
+
+## Cached Templates
+
+Cached/temporary files are stored in /tmp/makoserv in Ubuntu,  
+that directory is defined in the “temp” variable in line 14.  
+Change that when using makoser.py in other OSes.
---
+++ makoserv.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+""" 
+    Based on Mako-[version]/examples/wsgi/run_wsgi.py.
+    Changes marked with # S13:...
+"""
+import cgi, re, os, posixpath, sys
+from mako.lookup import TemplateLookup
+from mako import exceptions
+from mimetypes import guess_type
+
+root = os.getcwd()
+host = "0.0.0.0"        # S13: added this so it can be changed.
+port = 5000             # S13: used to be 8000, also changeable.
+temp = "/tmp/makoserv"  # S13: where to store cached templates.
+error_style = 'html' # select 'text' for plaintext error reporting
+
+lookup = TemplateLookup(directories=[root],
+    filesystem_checks=True, module_directory=temp,
+    # S13: because we'll be using some UTF-8 characters.
+    input_encoding="utf-8", output_encoding="utf-8")
+
+def serve(environ, start_response):
+    """serves requests using the WSGI callable interface."""
+    fieldstorage = cgi.FieldStorage(
+        fp = environ['wsgi.input'],
+        environ = environ,
+        keep_blank_values = True
+    )
+    # S13: template context has to be a bit more sensible.
+    d = dict()
+    d["args"] = dict([(k, getfield(fieldstorage[k])) for k in fieldstorage])
+    d["env"] = environ
+
+    uri = environ.get('PATH_INFO', '/')
+    if uri.endswith("/"):  # S13: use the index.html if no filename is given.
+        uri += 'index.html'
+    else:
+        uri = re.sub(r'^/$', '/index.html', uri)
+    
+    if re.match(r'.*\.(html|json)$', uri):    # S13: accept JSON requests.
+        try:
+            template = lookup.get_template(uri)
+            if uri.endswith(".json"):         # S13: include the corrent type.
+                start_response("200 OK", [('Content-Type','application/json')])
+            else:
+                start_response("200 OK", [('Content-Type','text/html')])
+            return [template.render(**d)]
+        except exceptions.TopLevelLookupException:
+            start_response("404 Not Found", [])
+            return ["Cant find template '%s'" % uri]
+        except:
+            if error_style == 'text':
+                start_response("200 OK", [('Content-Type','text/plain')])
+                return [exceptions.text_error_template().render()]
+            else:
+                start_response("200 OK", [('Content-Type','text/html')])
+                return [exceptions.html_error_template().render()]
+    else:
+        u = re.sub(r'^\/+', '', uri)
+        filename = os.path.join(root, u)
+        # S13: if the request is a file, proceed as usual.
+        if os.path.isfile(filename):
+            start_response("200 OK", [('Content-Type',
+                # S13: use mimetypes.guess_type.
+                guess_type(filename)[0])])
+            return [file(filename).read()]
+        # S13: if the request is a directory, redirect to the index.
+        elif os.path.isdir(filename):
+            uri += "/"
+            start_response("302 Moved Permanently", [("Location", uri)])
+            return []
+        # S13: if it's neither, respond with a 404.
+        else:
+            start_response("404 Not Found", [("Content-Type", "text/plain")])
+            return ["File not found."]
+ 
+def getfield(f):
+    """convert values from cgi.Field objects to plain values."""
+    if isinstance(f, list):
+        return [getfield(x) for x in f]
+    else:
+        return f.value
+
+# S13: there used to be a function guess_type here. Removed that
+# and simply replaced it with mimetype.guess_type (see imports).
+ 
+if __name__ == '__main__':
+    import wsgiref.simple_server
+    # S13: Allow us to change the host (name or IP) and port.
+    if len(sys.argv) > 1:
+        ipp = sys.argv[1].split(":")
+        if len(ipp) == 2:
+            host = ipp[0]
+            port = int(ipp[1])
+    server = wsgiref.simple_server.make_server(host, port, serve)
+    print "Server listening on port %d" % port
+    server.serve_forever()
+
+
